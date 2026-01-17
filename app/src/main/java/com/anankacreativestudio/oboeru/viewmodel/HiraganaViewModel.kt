d5924e9107750be59ecad44c3eb08cc998db096f
I Putu Andika Putra <andikaputraputu22@gmail.com>
2026-01-17 21:52:07 +0700

    Show bottom sheet for detail kana

--- app/src/main/java/com/anankacreativestudio/oboeru/viewmodel/HiraganaViewModel.kt
+++ app/src/main/java/com/anankacreativestudio/oboeru/viewmodel/HiraganaViewModel.kt
@@ -1,15 +1,27 @@
 package com.anankacreativestudio.oboeru.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.anankacreativestudio.oboeru.models.Kana
 import com.anankacreativestudio.oboeru.repository.KanaRepository
+import com.anankacreativestudio.oboeru.utils.SettingsPreferences
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class HiraganaViewModel @Inject constructor(
-    private val kanaRepository: KanaRepository
+    private val kanaRepository: KanaRepository,
+    private val settingsPreferences: SettingsPreferences
 ) : ViewModel() {
 
     val hiraganaList: List<Kana> = kanaRepository.hiraganaList
+
+    val soundEnabled = settingsPreferences.soundEnabled
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            true
+        )
 }
